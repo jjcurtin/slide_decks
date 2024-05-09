ab7831bdd5fb7a6871963df1121c4dac6c5d901c
John Curtin <jjcurtin@wisc.edu>
2024-05-09 10:27:03 -0500

    publish update

--- _presentations/2024/uwash/publish.sh
+++ _presentations/2024/uwash/publish.sh
@@ -1,18 +1,16 @@
 #!/bin/bash  
 
-# ./render.sh FILE FORMAT
-# FILE = all or filename
+# ./render.sh FORMAT
 # FORMAT = book, slides, or slides_wide
 
 FORMAT=$1
-FILE=$2
  
 if [ "$FORMAT" = "slides" ];  then
   echo ""
-	echo "Publishing $FILE to standard slides on quarto-pub"
+	echo "Publishing uwash.qmd to standard slides on quarto-pub"
   echo ""
   cp slides.css uwash.css 
-  quarto publish quarto-pub "$FILE" --no-browser
+  quarto publish quarto-pub uwash.qmd --no-browser
   rm -r *_files
   rm *.html
   git restore uwash.css 
@@ -20,10 +18,10 @@ fi
  
 if [ "$FORMAT" = "slides_wide" ];  then
   echo ""
-	echo "Publishing $FILE to wide slides on quarto-pub"
+	echo "Publishing uwash.qmd to wide slides on quarto-pub"
   echo ""
   cp slides_wide.css uwash.css 
-  quarto publish quarto-pub "$FILE" --no-browser
+  quarto publish quarto-pub uwash.qmd --no-browser
   rm -r *_files
   rm *.html
   git restore uwash.css 
