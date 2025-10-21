a4cf4ffaeaf296819dc1984a965398455029e1cc
John Curtin <jjcurtin@wisc.edu>
2025-10-21 11:12:08 -0500

    update for BoV talk

--- figs/risk2/figs_dev_probability.R
+++ figs/risk2/figs_dev_probability.R
@@ -2,43 +2,20 @@
 # Set up and load data
 ####################
 
+# These are from prelim analyses and should be updated as analyses are improved
 library(tidyverse)
 theme_set(theme_classic()) 
-path_data <- "data/risk" 
-path_mak <- "mak/risk"
+path_data <- "data/risk2" 
+path_mak <- "mak/risk2"
 
-# Load prediction data
-source(here::here(path_mak, "mak_probability.R"))  # create data if needed
-preds_week <- read_rds(here::here(path_data, "ema_preds_1week.rds"))
-preds_day <- read_rds(here::here(path_data, "ema_preds_1day.rds"))
-preds_hour <- read_rds(here::here(path_data, "ema_preds_1hour.rds")) 
-
-source(here::here(path_mak, "mak_ema_auroc_ci.R"))  # create data if needed
-auroc_ci <- read_csv(here::here(path_data, "ema_auroc_ci.csv"),
-                     show_col_types = FALSE) |>  
+auroc_ci <- read_csv(here::here(path_data, "ci_tmp.csv"),
+                     show_col_types = FALSE) |> 
+  bind_rows(tibble(median = NA_real_, lower = NA_real_, upper = NA_real_, 
+                   model = "Week")) |>
+  bind_rows(tibble(median = NA_real_, lower = NA_real_, upper = NA_real_,
+                   model = "Hour")) |>  
   mutate(model = factor(model, levels = c("Week", "Day", "Hour")))
 
-# Generate ROC curve data 
-roc_week <- preds_week |> 
-  yardstick::roc_curve(prob_beta, truth = label) |> 
-  mutate(model = "1week")
-  
-roc_day <- preds_day |> 
-  yardstick::roc_curve(prob_beta, truth = label) |> 
-  mutate(model = "1day")
-
-roc_hour <- preds_hour|> 
-  yardstick::roc_curve(prob_beta, truth = label) |> 
-  mutate(model = "1hour")
-
-roc_all <- roc_week |> 
-  bind_rows(roc_day) |> 
-  bind_rows(roc_hour) |> 
-  mutate(thres_avg = round(.threshold, 3)) |> 
-  group_by(model, thres_avg) |> 
-  reframe(sensitivity = mean(sensitivity), specificity =  mean(specificity)) |> 
-  mutate(model = factor(model, levels = c("1week", "1day", "1hour"),
-                        labels = c("week", "day", "hour")))
 
 ####################
 # plot functions
@@ -180,72 +157,6 @@ get_axis_labels <- function(x) {
 # Plots
 ####################
 
-# Histograms
-fig_hist_week <- preds_week |> 
-  plot_probs("week")
-
-# fig_week +
-#   geom_vline(xintercept = .5, color = "red", size = 2)
-
-
-# Calibration plot
-bin_width = 0.10
-fig_cal <- preds_day |> 
-  mutate(bins = cut(prob_beta, breaks = seq(0, 1, bin_width)),
-         lapse = if_else(label == "Lapse", 1, 0)) |> 
-  group_by(bins)  |> 
-  summarize(mean_lapse = mean(lapse),
-            .groups = "drop") |>
-  mutate(bins = as.numeric(bins),
-         midpoints = bin_width/2 + bin_width * (bins - 1))  |> 
-  ggplot(data = _, aes(x = midpoints, y = mean_lapse)) +
-    geom_abline(slope = 1, intercept = 0, linetype = "dashed",
-                color = "gray") +
-    geom_line(color = "green") +
-    geom_point(color = "green") +
-    labs(x = "Predicted Lapse Probability (bin mid-point)",
-         y = "Observed Lapse Probability") +
-    annotate("text", label = "Next day model", x = 0.2, y = 1,
-             color = "green", size = 6) +
-    scale_x_continuous(breaks = seq(0, 1, bin_width),
-                       limits = c(0, 1)) +
-    scale_y_continuous(breaks = seq(0, 1, bin_width),
-                       limits = c(0, 1)) +
-    theme(axis.text.x = element_text(size = 12),
-          axis.text.y = element_text(size = 12),
-          axis.title.x = element_text(size = 16),
-          axis.title.y = element_text(size = 16))
-
-# ROC curve plots
-line_colors  <- c("orange", "green", "blue")
-fig_roc_week <- roc_all |>
-  filter(model == "week") |>
-  plot_roc(line_colors) # +
-#   geom_text(x = .80, y = .20,
-#            label = str_c("AUC = ", auROC_week),
-#            show.legend = FALSE, color = "orange")
-
-fig_roc_week_day <- roc_all |>
-  filter(model == "week" | model == "day") |>
-  plot_roc(line_colors) # +
-#   geom_text(x = .80, y = .20,
-#            label = str_c("AUC = ", auROC_week),
-#            show.legend = FALSE, color = "orange") +
-#   geom_text(x = .80, y = .15,
-#             label = str_c("AUC = ", auROC_day),
-#            show.legend = FALSE, color = "green")
-
-fig_roc_all <- roc_all |>
-  plot_roc(line_colors) #  +
-#   geom_text(x = .80, y = .20,
-#            label = str_c("auROC = ", auROC_week),
-#            show.legend = FALSE, color = "orange") +
-#  geom_text(x = .80, y = .15,
-#            label = str_c("auROC = ", auROC_day),
-#            show.legend = FALSE, color = "green") +
-#  geom_text(x = .80, y = .10,
-#            label = str_c("auROC = ", auROC_hour),
-#            show.legend = FALSE, color = "blue")
 
 ###############################################################################
 # auROC CIs
